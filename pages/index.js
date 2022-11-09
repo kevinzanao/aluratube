77b29244669df2ac2a7614bba3705e8e2c8b450e
Kevin Zanao <kevinzanao2108@gmail.com>
2022-11-09 11:46:42 -0300

    dark mode implemented

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
@@ -15,7 +14,6 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
       <div style={estilosDaHomePage}>
         {/* Prop Drilling */}
         <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
@@ -40,6 +38,8 @@ export default HomePage
 // }
 
 const StyledHeader = styled.div `
+  background-color: ${({ theme }) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
